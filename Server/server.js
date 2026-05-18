db087e274d6a82755adc07f059f7976c53e9a689
Sundaran V <sundaranv@Sundarans-MacBook-Pro.local>
2026-05-18 23:46:15 +0530

    Added Netlify production URL to CORS origins

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://smart-brains-cubicle.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
